12006203c1b5484c595c49629ca69dc845908d93
guanyu.wu <guanyu.wu@orange.com>
2021-03-13 18:00:47 +0800

    add app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{message}}</h1>
+    <form @submit.prevent="postStudent">
+      <input type="text" v-model="nowInputedSutdentName" />
+      <button>add a student name</button>
+    </form>
+    <ul>
+      <li v-for="(stu, index) of  students" :key="index">
+        {{stu}}
+        <button @click="delStudent(index)">delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  data() {
+    return {
+      message: "hello vue",
+      students: [],
+      nowInputedSutdentName: ""
+    };
+  },
+  created() {
+    //test cors
+    // axios.get("http://127.0.0.1:7001/test").then(
+    //   res => {
+    //     console.log(res);
+    //     this.message = res.data;
+    //   },
+    //   err => {
+    //     console.log(err);
+    //   }
+    // );
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    //get students
+    this.getStudent();
+  },
+  methods: {
+    getStudent() {
+      axios.get("http://127.0.0.1:7001/students").then(
+        res => {
+          console.log(res);
+          this.students = res.data;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+    postStudent() {
+      axios
+        .post("http://127.0.0.1:7001/students", {
+          studentName: this.nowInputedSutdentName
+        })
+        .then(() => {
+          //reset  this.nowInputedSutdentName
+          this.nowInputedSutdentName = "";
+          this.getStudent();
+        });
+    },
+    //need checked later
+    delStudent(index) {
+      axios
+        .delete(`http://127.0.0.1:7001/students/${index}`)
+        .then(this.getStudent);
+    }
+  }
+};
+</script>
